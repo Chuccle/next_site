4a2084c47536a1326d92208d8f6c1a403b8cccc5
chuccle <charlie11cohen@gmail.com>
2023-07-09 00:06:17 +0100

    Added custom loading component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,20 @@ import * as THREE from "three";
 import styles from "/styles/Home.module.css";
 import React, { useRef, Suspense } from "react";
 import { Canvas, RootState, useFrame, useLoader, useThree } from "@react-three/fiber";
-import { Text, Loader, PerspectiveCamera } from "@react-three/drei";
+import { Text,  PerspectiveCamera} from "@react-three/drei";
 import Link from "next/link";
 import Image from "next/image";
+import { Html, useProgress } from '@react-three/drei'
 
 
 
+
+function Loader() {
+
+  const { progress } = useProgress();
+
+  return <Html center className={styles.IntroTitleText}>{progress} % loaded</Html>
+}
 
 
 function ResponsiveCamera(): JSX.Element {
@@ -215,45 +223,48 @@ export default function App(): JSX.Element {
 
             <div className={styles.background3D}>
 
-            <Suspense fallback={<Loader />}>
+
                 <Canvas shadows={true}>
 
-                    <ResponsiveCamera />
+                    <Suspense fallback={<Loader />}>
+                        <ResponsiveCamera />
 
-                    <Suspense fallback={<Space url="BufferTextures/galaxy_starfield_1024x512.png" />}>
-                        <Space url={"Model_Textures/galaxy_starfield.png"} />
-                    </Suspense>
+                        <Suspense fallback={<Space url="BufferTextures/galaxy_starfield_1024x512.png" />}>
+                            <Space url={"Model_Textures/galaxy_starfield.png"} />
+                        </Suspense>
 
-                    <directionalLight position={[1, 1, -1]} intensity={1} />
+                        <directionalLight position={[1, 1, -1]} intensity={1} />
 
-                    <Suspense fallback={
-                        <Moon
-                            urlTexture={"BufferTextures/moon1024x512.jpg"}
-                            urlNormalmap={"BufferTextures/moonNormal1024x512.jpg"}
-                        />}>
+                        <Suspense fallback={
+                            <Moon
+                                urlTexture={"BufferTextures/moon1024x512.jpg"}
+                                urlNormalmap={"BufferTextures/moonNormal1024x512.jpg"}
+                            />}>
 
-                        <Moon
-                            urlTexture={"Model_Textures/moon_4k_color_brim16.jpg"}
-                            urlNormalmap={"Model_Textures/moon_4k_normal.jpg"}
-                        />
-                    </Suspense>
+                            <Moon
+                                urlTexture={"Model_Textures/moon_4k_color_brim16.jpg"}
+                                urlNormalmap={"Model_Textures/moon_4k_normal.jpg"}
+                            />
+                        </Suspense>
 
-                    <Suspense fallback={<EarthClouds url={"BufferTextures/fair_clouds_1024x512.png"} />}>
-                        <EarthClouds url={"Model_Textures/fair_clouds_4k.png"} />
-                    </Suspense>
+                        <Suspense fallback={<EarthClouds url={"BufferTextures/fair_clouds_1024x512.png"} />}>
+                            <EarthClouds url={"Model_Textures/fair_clouds_4k.png"} />
+                        </Suspense>
 
-                    <Suspense fallback={
-                        <Earth
-                            urlTexture={"BufferTextures/basicTexture_1024x512.jpg"}
-                            urlBumpmap={"BufferTextures/bumpmap_1024x512.jpg"}
-                        />}>
-                        <Earth
-                            urlTexture={"Model_Textures/basicTexture.jpg"}
-                            urlBumpmap={"Model_Textures/bumpmap.jpg"}
-                        />
+                        <Suspense fallback={
+                            <Earth
+                                urlTexture={"BufferTextures/basicTexture_1024x512.jpg"}
+                                urlBumpmap={"BufferTextures/bumpmap_1024x512.jpg"}
+                            />}>
+                            <Earth
+                                urlTexture={"Model_Textures/basicTexture.jpg"}
+                                urlBumpmap={"Model_Textures/bumpmap.jpg"}
+                            />
+
+                        </Suspense>
                     </Suspense>
                 </Canvas>
-                </Suspense>
+
             </div>
             <section className={styles.accent}>
 
