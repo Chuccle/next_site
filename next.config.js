b1a22d388781e92413e38ae488b5cbbbb6d56539
chuccle <charlie11cohen@gmail.com>
2024-02-08 22:51:32 +0000

    Update dependencies, fixes to code, add prettier

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  concurrentFeatures: true,
+    reactStrictMode: true,
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
 import '../styles/globals.css'
-import React from 'react';
-import Head from "next/head";
-import Sidebar from "../components/sidebar";
-import Footer from "../components/footer";
-
+import React from 'react'
+import Head from 'next/head'
+import Sidebar from '../components/sidebar'
+import Footer from '../components/footer'
 
 function MyApp({ Component, pageProps }) {
-  return <> <React.StrictMode>
-              <Head>
-                <title>Home</title>
-                <link rel="icon" type="image/svg+xml" href="favicon.svg" />
-            </Head>
-          <Component {...pageProps}></Component> <Sidebar /> <Footer /> </React.StrictMode></>
+    return (
+        <>
+            <React.StrictMode>
+                <Head>
+                    <title>Home</title>
+                    <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+                </Head>
+                <Component {...pageProps}></Component> <Sidebar /> <Footer />
+            </React.StrictMode>
+        </>
+    )
 }
 
 export default MyApp
