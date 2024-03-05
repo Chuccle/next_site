e7adf7e55ece8c29cb85bfb373500170df4af19e
chuccle <charlie11cohen@gmail.com>
2024-03-05 22:16:47 +0000

    lenis stuff

--- components/space/composer.tsx
+++ components/space/composer.tsx
@@ -1,14 +1,15 @@
 
 import { useFrame, useThree } from '@react-three/fiber';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import * as THREE from 'three';
 import { Space } from './background';
 import Moon from './moon';
 import { Planet, PlanetClouds, OrbitLine } from './planet';
 import { Star } from './star';
-import useScrollBlock from '../utils';
 import { Stats } from '@react-three/drei';
 import SpaceText from './text';
+import { useLenis } from '@studio-freight/react-lenis';
+import { usePostProcess } from '../EffectComposer';
 
 //TODO: make spaceText underlined by orbit rings?
 // block scroll should maybe be longer?
@@ -24,8 +25,6 @@ export default function SpaceScene(): JSX.Element {
 
     const { camera } = useThree();
 
-    const [blockScroll, allowScroll] = useScrollBlock();
-
     const orbitRadiusEarth: number = 40;
     const orbitSpeedEarth: number = 22.5;
 
@@ -35,33 +34,14 @@ export default function SpaceScene(): JSX.Element {
     const orbitRadiusVenus: number = 30;
     const orbitSpeedVenus: number = 15;
 
-    useEffect(() => {
-        window.addEventListener('wheel', function (evt) {
-            ScrollEventHandler(evt, zoomOutRef);
-        });
-        window.addEventListener('keydown', function (evt) {
-            ScrollEventHandler(evt, zoomOutRef);
-        });
-        window.addEventListener('touchmove', function (evt) {
-            ScrollEventHandler(evt, zoomOutRef);
-        });
-        blockScroll();
+    useLenis(() => {
+        // called every scroll
+        zoomOutRef.current += 0.0005;
+ 
+    });
 
-        return () => {
-            window.removeEventListener('wheel', function (evt) {
-                ScrollEventHandler(evt, zoomOutRef);
-            });
-            window.removeEventListener('keydown', function (evt) {
-                ScrollEventHandler(evt, zoomOutRef);
-            });
-            window.removeEventListener('touchmove', function (evt) {
-                ScrollEventHandler(evt, zoomOutRef);
-            });
-            allowScroll();
-        };
-
-    }, [blockScroll, allowScroll]);
-
+    usePostProcess();
+  
     // So we don't constantly allocate per frame
     const planetPosition: THREE.Vector3 = new THREE.Vector3();
     const starPosition: THREE.Vector3 = new THREE.Vector3();
@@ -106,7 +86,6 @@ export default function SpaceScene(): JSX.Element {
                 targetCameraPosition = [0, 80, 0];
                 // share the pointer please
                 targetLookAt = starPosition;
-                allowScroll();
             }
 
             // Interpolate camera position and look-at point
@@ -118,130 +97,130 @@ export default function SpaceScene(): JSX.Element {
     });
     return (
         <>
-            <color attach='background' args={["black"]} />
-            <Stats />
+                <color attach='background' args={["black"]} />
+                <Stats />
 
-            <Space starCount={500} >
-                <pointLight decay={0} castShadow={true} />
-                {/* <OrbitControls/> */}
-                <SpaceText
-                    font={'/fonts/helvetiker_bold.json'}
-                    matcapTextureName={'CB4E88_F99AD6_F384C3_ED75B9'}
-                    textToDisplay={" Software\n    out of\n this world"}
-                    size={9}
-                    height={1}
-                    scale={[1, 1, 1]}
-                    position={[-31, 50, 0]}
-                    rotation={[-1.5, 0, 0]}
-                    curveSegments={24}
-                    bevelsEnabled
-                    bevelSegments={1}
-                    bevelSize={0.008}
-                    bevelThickness={0.03}
-                    lineHeight={1}
-                    letterSpacing={0.2}
-                />
-                <Star
-                    rotationSpeed={0.00035}
-                    receiveshadow={false}
-                    castshadow={false}
-                    meshArgs={[10, 30, 30]}
-                    position={[0.0, 0.0, 0.0]}
-                    urlTexture="Model_Textures/2k_sun.jpg"
-                    passedMeshRef={starRef}
-                />
-                <OrbitLine radius={orbitRadiusMercury} passedMeshRef={orbitRefGroup[0]} />
-                <Planet
-                    type='standard'
-                    rotationSpeed={0.0045}
-                    receiveshadow={true}
-                    castshadow={false}
-                    meshArgs={[1.5, 30, 30]}
-                    position={[0.0, 0.0, 0.0]}
-                    orbitDistance={orbitRadiusMercury}
-                    orbitSpeed={orbitSpeedMercury}
-                    urlTexture='Model_Textures/2k_mercury.jpg'
-                />
-                <OrbitLine radius={orbitRadiusVenus} passedMeshRef={orbitRefGroup[1]} />
-                <Planet
-                    type='standard'
-                    rotationSpeed={0.0030}
-                    receiveshadow={true}
-                    castshadow={false}
-                    meshArgs={[2.5, 30, 30]}
-                    position={[0.0, 0.0, 0.0]}
-                    orbitDistance={orbitRadiusVenus}
-                    orbitSpeed={orbitSpeedVenus}
-                    urlTexture='Model_Textures/2k_venus_atmosphere.jpg'
-                />
-                <Moon
-                    rotationSpeed={0.015}
-                    receiveshadow={false}
-                    castshadow={false}
-                    meshArgs={[0.5, 15, 15]}
-                    position={[0.0, 0.0, 0.0]}
-                    orbitDistance={5}
-                    orbitSpeed={3}
-                    urlTexture="Model_Textures/nasa_moon_4k.jpg"
-                    planetOrbitDistance={orbitRadiusEarth}
-                    planetOrbitSpeed={orbitSpeedEarth}
-                />
-                <PlanetClouds
-                    rotationSpeed={0.0025}
-                    receiveshadow={true}
-                    castshadow={false}
-                    meshArgs={[3.55, 30, 30]}
-                    position={[0.0, 0.0, 0.0]}
-                    orbitDistance={orbitRadiusEarth}
-                    orbitSpeed={orbitSpeedEarth}
-                    urlTexture="Model_Textures/fair_clouds_4k.png"
-                />
-                <OrbitLine radius={orbitRadiusEarth} passedMeshRef={orbitRefGroup[2]} />
-                <Planet
-                    type='bumpyAndReflective'
-                    urlTexture='Model_Textures/basicTexture.jpg'
-                    rotationSpeed={0.0015}
-                    receiveshadow={true}
-                    castshadow={false}
-                    meshArgs={[3.5, 30, 30]}
-                    position={[0.0, 0.0, 0.0]}
-                    orbitDistance={orbitRadiusEarth}
-                    orbitSpeed={orbitSpeedEarth}
-                    passedMeshRef={planetRef}
-                    urlBumpmap='Model_Textures/bumpmap.jpg'
-                    bumpScale={15}
-                    urlSpecularmap="Model_Textures/water_4k.png"
-                    reflectivity={1}
-                />
-            </Space>
-            {/* <ambientLight></ambientLight> */}
+                <Space starCount={500} >
+                    <pointLight decay={0} castShadow={true} />
+                    {/* <OrbitControls/> */}
+                    <SpaceText
+                        font={'/fonts/helvetiker_bold.json'}
+                        matcapTextureName={'CB4E88_F99AD6_F384C3_ED75B9'}
+                        textToDisplay={" Software\n    out of\n this world"}
+                        size={9}
+                        height={1}
+                        scale={[1, 1, 1]}
+                        position={[-31, 50, 0]}
+                        rotation={[-1.5, 0, 0]}
+                        curveSegments={24}
+                        bevelsEnabled
+                        bevelSegments={1}
+                        bevelSize={0.008}
+                        bevelThickness={0.03}
+                        lineHeight={1}
+                        letterSpacing={0.2}
+                    />
+                    <Star
+                        rotationSpeed={0.00035}
+                        receiveshadow={false}
+                        castshadow={false}
+                        meshArgs={[10, 30, 30]}
+                        position={[0.0, 0.0, 0.0]}
+                        urlTexture="Model_Textures/2k_sun.jpg"
+                        passedMeshRef={starRef}
+                    />
+                    <OrbitLine radius={orbitRadiusMercury} passedMeshRef={orbitRefGroup[0]} />
+                    <Planet
+                        type='standard'
+                        rotationSpeed={0.0045}
+                        receiveshadow={false}
+                        castshadow={false}
+                        meshArgs={[1.5, 30, 30]}
+                        position={[0.0, 0.0, 0.0]}
+                        orbitDistance={orbitRadiusMercury}
+                        orbitSpeed={orbitSpeedMercury}
+                        urlTexture='Model_Textures/2k_mercury.jpg'
+                    />
+                    <OrbitLine radius={orbitRadiusVenus} passedMeshRef={orbitRefGroup[1]} />
+                    <Planet
+                        type='standard'
+                        rotationSpeed={0.0030}
+                        receiveshadow={false}
+                        castshadow={false}
+                        meshArgs={[2.5, 30, 30]}
+                        position={[0.0, 0.0, 0.0]}
+                        orbitDistance={orbitRadiusVenus}
+                        orbitSpeed={orbitSpeedVenus}
+                        urlTexture='Model_Textures/2k_venus_atmosphere.jpg'
+                    />
+                    <Moon
+                        rotationSpeed={0.015}
+                        receiveshadow={false}
+                        castshadow={false}
+                        meshArgs={[0.5, 15, 15]}
+                        position={[0.0, 0.0, 0.0]}
+                        orbitDistance={5}
+                        orbitSpeed={3}
+                        urlTexture="Model_Textures/nasa_moon_4k.jpg"
+                        planetOrbitDistance={orbitRadiusEarth}
+                        planetOrbitSpeed={orbitSpeedEarth}
+                    />
+                    <PlanetClouds
+                        rotationSpeed={0.0025}
+                        receiveshadow={false}
+                        castshadow={false}
+                        meshArgs={[3.55, 30, 30]}
+                        position={[0.0, 0.0, 0.0]}
+                        orbitDistance={orbitRadiusEarth}
+                        orbitSpeed={orbitSpeedEarth}
+                        urlTexture="Model_Textures/fair_clouds_4k.png"
+                    />
+                    <OrbitLine radius={orbitRadiusEarth} passedMeshRef={orbitRefGroup[2]} />
+                    <Planet
+                        type='bumpyAndReflective'
+                        urlTexture='Model_Textures/basicTexture.jpg'
+                        rotationSpeed={0.0015}
+                        receiveshadow={false}
+                        castshadow={false}
+                        meshArgs={[3.5, 30, 30]}
+                        position={[0.0, 0.0, 0.0]}
+                        orbitDistance={orbitRadiusEarth}
+                        orbitSpeed={orbitSpeedEarth}
+                        passedMeshRef={planetRef}
+                        urlBumpmap='Model_Textures/bumpmap.jpg'
+                        bumpScale={15}
+                        urlSpecularmap="Model_Textures/water_4k.png"
+                        reflectivity={1}
+                    />
+                </Space>
+                {/* <ambientLight></ambientLight> */}
         </>
     );
 }
 
-function ScrollEventHandler(evt: Event, zoomOutRef: React.MutableRefObject<number>) {
-    switch (evt.type) {
-        case "wheel": {
-            const wheelEvt = evt as WheelEvent;
-            if (wheelEvt.deltaY > 0) {
-                // Scrolling down
-                zoomOutRef.current += 0.005;
-            }
-            break;
-        }
-        case "keydown": {
-            const keyEvt = evt as KeyboardEvent;
-            if (keyEvt.key === "ArrowDown") {
-                // Pressing the down arrow key
-                zoomOutRef.current += 0.005;
-            }
-            break;
-        }
-        case "touchmove": {
-            // TODO: Look at making this only occur on drag up on touchscreen devices
-            zoomOutRef.current += 0.0025;
-            break;
-        }
-    }
-}
+// function ScrollEventHandler(evt: Event, zoomOutRef: React.MutableRefObject<number>) {
+//     switch (evt.type) {
+//         case "wheel": {
+//             const wheelEvt = evt as WheelEvent;
+//             if (wheelEvt.deltaY > 0) {
+//                 // Scrolling down
+//                 zoomOutRef.current += 0.005;
+//             }
+//             break;
+//         }
+//         case "keydown": {
+//             const keyEvt = evt as KeyboardEvent;
+//             if (keyEvt.key === "ArrowDown") {
+//                 // Pressing the down arrow key
+//                 zoomOutRef.current += 0.005;
+//             }
+//             break;
+//         }
+//         case "touchmove": {
+//             // TODO: Look at making this only occur on drag up on touchscreen devices
+//             zoomOutRef.current += 0.0025;
+//             break;
+//         }
+//     }
+// }
 
